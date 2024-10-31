ebaa4812f5f5c3629cc03df3a13da79d9bdc37e9
Rajesh Das <rajesh1585700@gmail.com>
2024-11-01 01:05:50 +0530

    Fix range input not working issur

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect, useCallback } from "react"
+import { useState, useEffect, useCallback, useRef } from "react"
 
 function App() {
   const [length, setLength] = useState(8)
   const [hasNumericChar, setHasNumericChar] = useState(false)
   const [hasSpecialChar, setHasSpecialChar] = useState(false)
   const [password, setPassword] = useState("")
+
+  const passwordRef=useRef()
 
   const generatePassword=useCallback(()=>{
     let charStr="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -19,6 +21,10 @@ function App() {
   }, [length, hasNumericChar, hasSpecialChar])
   useEffect(()=>{generatePassword()}, [length, hasNumericChar, hasSpecialChar])
 
+  const copyHandler=()=>{
+    window.navigator.clipboard.writeText(passwordRef.current.value);
+  }
+
   return (
     <>
      <div>
@@ -30,12 +36,12 @@ function App() {
       <div className="flex justify-center items-center h-96">
        <div className="border-2 p-8 shadow-xl">
         <div className="flex py-3 justify-around">
-          <input type="text" placeholder="Password" value={password} className="text-lg box-border border-2 rounded-lg w-9/12 px-3"></input>
-          <button className="bg-green-500 box-border px-2 rounded-lg text-white text-lg w-2/12">Copy</button>
+          <input type="text" ref={passwordRef} placeholder="Password" value={password} className="text-lg box-border border-2 rounded-lg w-9/12 px-3"></input>
+          <button className="bg-green-500 box-border px-2 rounded-lg text-white text-lg w-2/12 hover:opacity-80" onClick={copyHandler}>Copy</button>
         </div>
         <div className="flex content-center">
           <label htmlFor="length">Length: {length}</label>
-          <input type="range" min={8} max={100} id="length" className="mx-1" onClick={(e)=>{setLength(e.target.value)}}></input>
+          <input type="range" min={8} max={100} id="length" className="mx-1" onChange={(e)=>{setLength(e.target.value)}}></input>
           <input type="checkbox" id="isNumeric" onClick={(e)=>{setHasNumericChar(e.target.checked)}}></input>
           <label htmlFor="isNumeric" className="mx-1">Numeric Characters</label>
           <input type="checkbox" id="isSpecialChar" onClick={(e)=>{setHasSpecialChar(e.target.checked)}}></input>
